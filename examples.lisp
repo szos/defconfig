4e4da5469ea93d423ac1438bfe15388597c843d5
szos <szos@posteo.net>
2020-12-28 23:26:23 +0100

    expanded upon examples

--- examples.lisp
+++ examples.lisp
@@ -1,5 +1,11 @@
 (in-package :defcustom)
 
+;; the simplest example:
+(defconfig *varname 'dark)
+;; the above would expand into a defvar form and a config-info instance, which validates using 'identity making
+;; every value valid.
+
+;; more complexe examples. 
 (defconfig *varname* 'dark :valid-values '(dark light) :test 'eql :reinitialize t)
 ;; the above would expand into a defparameter form and config-info which validates 'dark against the list
 ;; '(dark light) using comparison-fn.
@@ -18,9 +24,10 @@
 ;; The above would expand into a config-info instance which validates against the list '(dark light) using 'eql.
 ;; because reinitialize is provided, it will also include the form (setf (input-bar-color *default-input-bar*) 'dark)
 ;; this will only validate when the accessor input-bar-color is called on the argument *default-input-bar*
-(defconfig (input-bar-color *default-input-bar*) 'dark :predicate 'symbolp)
+(defconfig (input-bar-color) 'dark :predicate 'symbolp)
 ;; the above would expand into a config-info instance which validates using 'symbolp, and doesnt include a setf form
-;; this will only validate when the accessor input-bar-color is called on the argument *default-input-bar*
+;; this will only validate when the accessor input-bar-color is called on an argument that isnt *default-input-bar*
+;; assuming that no other defconfig forms have been defined. 
 (defconfig (input-bar-location) :top :valid-values
   '(member :top :bottom :left :right) :coercer 'location-sym-coercer
   :documentation "a variable for locations on screen" :tags '("input-bar" "bar" "input" "location"))
@@ -29,6 +36,19 @@
 ;; it will be valid for ALL forms beginning (setf (input-bar-location ...) ...), as opposed to the previous examples
 ;; which are only valid when called with the form (setf (input-bar-color *default-input-bar*) ...)
 
+;;; When determining which object to use to validate when setting using an accessor we want to first check if the
+;;; specific form has a validator. using the above as an example, if we call
+;;; (set-valid (input-bar-color *mybar*) 'red), we will first look to see if '(input-bar-color *mybar*) is a key
+;;; in the accessors hash table, and if so use it. if not, we then look to see if 'input-bar-color is a key in the
+;;; same table (or perhaps a different table? db representation still needs to be decided upon) and if so use that.
+;;; so for the above examples here is the behavioure:
+;;; (set-valid (input-bar-color *default-input-bar*) 'red) - this will be invalid
+;;; (set-valid (input-bar-color *mybar*) 'red) - this will be valid. 
+
+;;; Regarding documentation:
+;;; Should documentation be provided in a parameter/var producing defconfig form then the documentation should be
+;;; both spliced in to the def* form AND added to the config-info object.
+;;; should it be provided in an accessor defconfig, it will only be added to the config-info object. 
 
 
 
