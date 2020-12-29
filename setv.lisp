32a6bc77df3c85a8860855555526fc829b73400f
szos <szos@posteo.net>
2020-12-29 02:24:21 +0100

    added setv macro and helper macros/Funcitons.

--- setv.lisp
+++ setv.lisp
@@ -1,2 +1,46 @@
 (in-package :defconfig)
 
+(defun remove-keys (list keys-to-remove)
+  "list must be an even length"
+  (let ((return-list nil)
+	(removed-keys nil))
+    (loop for (k v) on list by 'cddr
+	  if (member k keys-to-remove)
+	    do (push k removed-keys)
+	       (push v removed-keys)
+	  else do (push k return-list)
+		  (push v return-list))
+    (values (reverse return-list)
+	    (reverse removed-keys))))
+
+(defmacro %setv (place value db)
+  (alexandria:with-gensyms (hold hash config-info valid? coer-hold coer-valid?)
+    `(let* ((,hold ,value)
+	    (,hash ,(if (listp place) `(car ,db) `(cdr ,db)))
+	    (,config-info
+	      ,(if (listp place)
+		   `(or (gethash ',place ,hash)
+			(gethash ',(car place) ,hash)
+			(error 'no-config-found-error :place ',place))
+		   `(or (gethash ',place ,hash)
+			(error 'no-config-found-error :place ',place))))
+	    (,valid? (funcall (config-info-predicate ,config-info) ,hold)))
+       (cond (,valid? (setf ,place ,hold))
+	     ((config-info-coercer ,config-info)
+	      (let* ((,coer-hold (funcall (config-info-coercer ,config-info) ,hold))
+		     (,coer-valid? (funcall (config-info-predicateo ,config-info) ,coer-hold)))
+		(if ,coer-valid?
+		    (setf ,place ,coer-hold)
+		    (error 'invalid-coerced-datum-error :place ',place :value ,hold :coerced-value ,coer-hold))))
+	     (t (error 'invalid-datum-error :place ',place :value ,hold))))))
+
+(defmacro setv (&rest args)
+  "Setv must get an even number of args - every place must have a value"
+  (multiple-value-bind (pairs database) (remove-keys args '(:db))
+    `(progn ,@(loop for (place value) on pairs by 'cddr
+		    collect `(%setv ,place ,value ,@(if database (cdr database) '(*default-db*)))))))
+
+;; (setv *x* 2
+;;       *y* 3
+;;       (accessor *var*) 3
+;;       :db *my-db*)
