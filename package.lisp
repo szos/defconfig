f61d5712958f511c51a5d9ab95931432a58651b8
szos <szos@posteo.net>
2020-12-30 06:18:00 +0100

    export symbols

--- package.lisp
+++ package.lisp
@@ -1,4 +1,13 @@
 ;;;; package.lisp
 
 (defpackage #:defconfig
-  (:use #:cl))
+  (:use #:cl)
+  (:export defconfig
+	   setv
+	   place->config-info
+	   config-info-search
+	   *default-db*
+	   invalid-datum-error
+	   invalid-coerced-datum-error
+	   no-config-found-error
+	   make-config-database))
