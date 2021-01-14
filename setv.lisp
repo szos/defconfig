ec1b85e03d2043db07cd3c5588d2010e3a7e52a8
Nathan Shostek <szos@posteo.net>
2021-01-14 15:34:54 +0100

    rewrote %setv, modified with-atomic-setv
    %setv was rewritten - broke out several parts for readability. this
    could use more work, to really break out the portions that could use
    it. in other words we made a mess and we need to clean up.
    
    with-atomic-setv was modified to re-error if we error out of the
    block, and if not we return the return value from body.

--- setv.lisp
+++ setv.lisp
@@ -33,91 +33,120 @@ destructuring-bind."
 	   ,key-hold
 	 ,@body))))
 
+(defmacro %setv-coerced (validity config-info-object place original-value coerced-value)
+  `(restart-case
+       (if ,validity
+	   (progn (psetf (config-info-prev-value ,config-info-object) ,place
+			 ,place ,coerced-value)
+		  ,coerced-value)
+	   (error 'invalid-coerced-datum-error
+		  :place ',place :value ,original-value :coerced-value ,coerced-value))
+     (set-place-to-coerced-value ()
+       :report (lambda (stream)
+		 (format stream "Set ~S to ~S" ',place ,coerced-value))
+       (psetf (config-info-prev-value ,config-info-object) ,place
+	      ,place ,coerced-value)
+       ,coerced-value)))
+
+(defmacro %setv-validity-set (validity config-info-object place value)
+  (alexandria:with-gensyms (coer-hold coer-valid?)
+    `(restart-case
+	 (cond (,validity
+		(psetf (config-info-prev-value ,config-info-object) ,place
+		       ,place ,value)
+		,value)
+	       ((config-info-coercer ,config-info-object)
+		(let* ((,coer-hold (funcall (config-info-coercer ,config-info-object) ,value))
+		       (,coer-valid? (funcall (config-info-predicate ,config-info-object) ,coer-hold)))
+		  (%setv-coerced ,coer-valid? ,config-info-object ,place ,value ,coer-hold)))
+	       (t (error 'invalid-datum-error :place ',place :value ,value)))
+       (set-place-to-value ()
+	 :report (lambda (s)
+		   (format s "Set ~S to ~S" ',place ,value))
+	 (psetf (config-info-prev-value ,config-info-object) ,place
+		,place ,value)
+	 ,value))))
+
 (defmacro %setv (place value db)
-  (alexandria:with-gensyms (hold hash config-info valid? coer-hold coer-valid?)
+  "Validates value against the config-info object looked up in db using place. 
+value will be evaluated first, then the config-info object will be looked up.
+After that the value will be checked against the registered predicate. If value
+is valid, place is set to it, and the config info object will update its 
+previous value slot. otherwise coercion is attempted and, if successful, place
+gets set to the coerced value. Restarts are put into place "
+  (alexandria:with-gensyms (hold hash config-info-object valid?)
     `(let* ((,hold ,value)
 	    (,hash ,(if (listp place) `(car ,db) `(cdr ,db)))
-	    (,config-info
+	    (,config-info-object
 	      ,(if (listp place)
 		   `(or (gethash ',place ,hash)
 			(gethash ',(car place) ,hash)
 			(error 'no-config-found-error :place ',place :db ',db))
 		   `(or (gethash ',place ,hash)
 			(error 'no-config-found-error :place ',place :db ',db))))
-	    (,valid? (funcall (config-info-predicate ,config-info) ,hold)))
-       (restart-case
-	   (cond (,valid? (psetf (config-info-prev-value ,config-info) ,place
-				 ,place ,hold))
-		 ((config-info-coercer ,config-info)
-		  (let* ((,coer-hold (funcall (config-info-coercer ,config-info) ,hold))
-			 (,coer-valid? (funcall (config-info-predicate ,config-info) ,coer-hold)))
-		    (restart-case 
-			(if ,coer-valid?
-			    (psetf (config-info-prev-value ,config-info) ,place
-				   ,place ,coer-hold)
-			    (error 'invalid-coerced-datum-error
-				   :place ',place :value ,hold :coerced-value ,coer-hold))
-		      (set-place-to-coerced-value ()
-			:report (lambda (stream)
-				  (format stream "Set ~S to ~S" ',place ,coer-hold))
-			(psetf (config-info-prev-value ,config-info) ,place
-			       ,place ,coer-hold)))))
-		 (t (error 'invalid-datum-error :place ',place :value ,hold)))
-	 (set-place-to-value ()
-	   :report (lambda (stream)
-		     (format stream "Set ~S to ~S" ',place ,hold))
-	   (psetf (config-info-prev-value ,config-info) ,place
-		  ,place ,hold))))))
+	    (,valid? (funcall (config-info-predicate ,config-info-object) ,hold)))
+       (%setv-validity-set ,valid? ,config-info-object ,place ,hold))))
 
 (defmacro setv (&rest args)
-  "Setv must get an even number of args - every place must have a value"
+  "Setv must get an even number of args - every place must have a value. Setv
+can also take the key argument :db, to specify which database to look up config
+objects in. "
   (destructuring-keys (pairs (:db '*default-db*)) args
     `(progn ,@(loop for (p v) on pairs by 'cddr
 		    collect `(%setv ,p ,v ,db)))))
 
 (defmacro setv-atomic (&rest args)
   "this version of setv saves the original value of the places being set, and resets all to their original 
-value if an error is encountered."
-  (multiple-value-bind (pairs db) (remove-keys args '(:db))
-    (let ((syms (loop for (p v) on pairs by 'cddr collect (gensym))))
-      `(let ,(loop for (place value) on pairs by 'cddr
-		   for gensym in syms
-		   collect `(,gensym ,place))
-	 (handler-case
-	     (progn ,@(loop for (place value) on pairs by 'cddr
-			    collect `(%setv ,place ,value ,@(if db (cdr db) '(*default-db*)))))
-	   (error (c)
-	     ,@(loop for (place value) on pairs by 'cddr
+value if an error is encountered. the error is then resignalled"
+  (alexandria:with-gensyms (c)
+    (multiple-value-bind (pairs db) (remove-keys args '(:db))
+      (let ((syms (loop for (p v) on pairs by 'cddr collect (gensym))))
+	`(let ,(loop for (place value) on pairs by 'cddr
 		     for gensym in syms
-		     collect `(setf ,place ,gensym))
-	     c))))))
+		     collect `(,gensym ,place))
+	   (handler-case
+	       (progn ,@(loop for (place value) on pairs by 'cddr
+			      collect `(%setv ,place ,value ,@(if db (cdr db) '(*default-db*)))))
+	     (error (,c)
+	       ,@(loop for (place value) on pairs by 'cddr
+		       for gensym in syms
+		       collect `(setf ,place ,gensym))
+	       (error ,c))))))))
 
 (defmacro %atomic-setv-reset (db)
+  "this macro resets all encountered places within a call to with-atomic-setv."
   (declare (special *setv-place-accumulator*))
   (let ((place-list (cdr *setv-place-accumulator*)))
     `(progn ,@(loop for place in place-list
 		    collect `(reset-place ,place :db ,db :previous-value t)))))
 
 (defmacro %setv-with-reset (block-name place value db)
+  "Wrap setv in a handler to catch all errors, which will reset all encountered
+ places, after which it returns the condition from the named block."
   (declare (special *setv-place-accumulator*))
   (push place *setv-place-accumulator*)
-  `(handler-case (%setv ,place ,value ,db)
-     (error ()
-       (%atomic-setv-reset ,db)
-       (return-from ,block-name))))
+  (alexandria:with-gensyms (c)
+    `(handler-case (%setv ,place ,value ,db)
+       (error (,c)
+	 (%atomic-setv-reset ,db)
+	 (return-from ,block-name ,c)))))
 
 (defmacro %atomic-setv (block-name &rest args)
+  "generates a set of calls to %setv-with-reset."
   (declare (special *setv-place-accumulator*))
   (destructuring-keys (pairs (:db '*default-db*)) args
     `(progn ,@(loop for (p v) on pairs by 'cddr
 		    collect `(%setv-with-reset ,block-name ,p ,v ,db)))))
 
-(defmacro with-atomic-setv (() &body body)
+(defmacro with-atomic-setv ((&key (re-error t)) &body body)
   "This macro causes every call to setv to, on an invalid value, reset the place in 
 question to its previous value."
-  (alexandria:with-gensyms (args block-name)
+  (alexandria:with-gensyms (args block-name c)
     `(compiler-let ((*setv-place-accumulator* nil))
-       (block ,block-name
-	 (macrolet ((setv (&rest ,args)
-		      `(%atomic-setv ,',block-name ,@,args)))
-	   ,@body)))))
+       (let ((,c (block ,block-name
+		   (macrolet ((setv (&rest ,args)
+				`(%atomic-setv ,',block-name ,@,args)))
+		     ,@body))))
+	 (if (and (typep ,c 'error) ,re-error)
+	     (error 'atomic-setv-wrapped-error :error ,c)
+	     ,c)))))
