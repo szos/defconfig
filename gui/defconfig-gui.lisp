b4a7aea8115c9d88f100a4d87aeeaac75b20c441
Nathan Shostek <szos@posteo.net>
2021-01-27 00:18:51 +0100

    added macros, formatting, buttons, and more
    added the macro monospaced, which wraps with-text-family with the
    :fix family selected.
    
    added change-layout macro which takes a frame and a layout and checks
    if that layout is already set before changing to it.
    
    added reinitialize and regen-config to the defconfig calls.
    
    added current-config-info slot to the application frame
    
    added config-info-view pane and layout for it
    
    added app-main function to ease running the app
    
    changed com-refresh-frame to be named "Refresh"
    
    added presentation type, command, and command-translator to switch to
    the config-info oriented layout.
    
    modified write-database-to-stream to show information about the
    symbol/accessor in a better manner.
    
    added functions to display a config info object. these should include
    buttons to reset values, but they dont refresh the frame afterwards
    despite calls to com-refresh-frame.

--- gui/defconfig-gui.lisp
+++ gui/defconfig-gui.lisp
@@ -10,9 +10,17 @@
   `(with-text-face (,stream :italic)
      ,@body))
 
+(defmacro monospaced ((stream) &body body)
+  `(with-text-family (,stream :fix)
+     ,@body))
+
 (defmacro format-color (stream ink control-string &rest args)
   `(with-drawing-options (,stream :ink ,ink)
      (format ,stream ,control-string ,@args)))
+
+(defmacro change-layout (frame layout)
+  `(unless (equal (frame-current-layout ,frame) ,layout)
+     (setf (frame-current-layout ,frame) ,layout)))
 
 (define-defconfig-db *gui-db* :defconfig-gui
   :parameter nil
@@ -22,14 +30,18 @@
   :typespec 'climi::standard-color
   :db *gui-db*
   :tags '("database-color" "database-display-color" "color")
-  :documentation "the color with which to display database names")
+  :documentation "the color with which to display database names"
+  :reinitialize t :regen-config t)
 
 (defconfig *config-symbol-color* +dark-sea-green+ :typespec 'climi::standard-color
   :db *gui-db*
   :tags '("config" "symbol" "color")
-  :documentation "the color with which to display config symbols")
+  :documentation "the color with which to display config symbols"
+  :reinitialize t :regen-config t)
 
-(define-application-frame defconfig-configurator () ()
+(define-application-frame defconfig-configurator ()
+  ((current-config-info :initform nil
+			:accessor configurator-current-config-info))
   (:panes
    (database-list :application
 		  :display-function 'display-database-list
@@ -41,6 +53,11 @@
 		  :scroll-bars nil
 		  :width :compute
 		  :height :compute)
+   (config-info-view :application
+		     :display-function 'display-config-info
+		     :scroll-bars nil
+		     :width :compute
+		     :height :compute)
    (interactor :interactor))
   (:layouts
    (default
@@ -49,11 +66,20 @@
       (make-pane 'clim-extensions:box-adjuster-gadget)
       (:fill (scrolling () database-view))
       (make-pane 'clim-extensions:box-adjuster-gadget)
+      (1/4 interactor)))
+   (inspect-config-info
+    (vertically ()
+      (20 database-list)
+      (make-pane 'clim-extensions:box-adjuster-gadget)
+      (:fill (scrolling () config-info-view))
+      (make-pane 'clim-extensions:box-adjuster-gadget)
       (1/4 interactor)))))
 
-(define-defconfig-configurator-command (com-refresh-frame :name t)
-    ()
-  ())
+(defun app-main ()
+  (run-frame-top-level (make-application-frame 'defconfig-configurator)))
+
+(define-defconfig-configurator-command (com-refresh-frame :name "Refresh")  () ())
+
 (define-defconfig-configurator-command (com-quit :name t) ()
   (frame-exit *application-frame*))
 
@@ -64,7 +90,8 @@
   (let* ((dbp (defconfig::getdb database))
 	 (name (car dbp))
 	 (db (cdr dbp)))
-    (set-display-db database name db)))
+    (set-display-db database name db))
+  (change-layout *application-frame* 'default))
 
 (define-presentation-to-command-translator inspect-database
     (database-list-presentation com-inspect-database defconfig-configurator
@@ -86,36 +113,52 @@
 		 (format pane ")"))
 	       (format pane ", ")))))
 
-(defun write-database-to-stream (pane db)
+(define-presentation-type inpect-config-info-presentation ())
+(define-presentation-to-command-translator inspect-config-info
+    (inpect-config-info-presentation com-inspect-config-info
+     defconfig-configurator :gesture :select
+			    :documentation "Inspect the configuration object")
+    (obj)
+  (list obj))
+
+(define-defconfig-configurator-command (com-inspect-config-info :name t)
+    ((config-info defconfig::config-info))
+  (setf (configurator-current-config-info *application-frame*) config-info)
+  (unless (equal (frame-current-layout *application-frame*) 'inspect-config-info)
+    (setf (frame-current-layout *application-frame*) 'inspect-config-info)))
+
+(defun hash-table-empty-p (hash-table)
+  (case (hash-table-count hash-table)
+    (0 t)
+    (otherwise nil)))
+
+(defun write-database-to-stream (pane db &optional accessor)
   (slim:with-table (pane)
     (bold (pane)
       (slim:row
 	(slim:cell
-	  (format pane "Config Variable"))
+	  (format pane "Package"))
 	(slim:cell
-	  (format pane "Current Value"))
+	  (format pane (if accessor "Accessor" "Variable")))
 	(slim:cell
-	  (format pane "Previous Value"))
-	(slim:cell
-	  (format pane "Default Value"))
-	(slim:cell
-	  (format pane "Valid Values"))))
-    (if (hash-table-p db)
-	(maphash
-	 (lambda (k v)
-	   (slim:row
-	     (slim:cell 
-	       (format-color pane *config-symbol-color* "~S" k))
-	     (slim:cell
-	       (format pane "~S" (symbol-value k)))
-	     (slim:cell
-	       (format pane "~S" (config-info-prev-value v)))
-	     (slim:cell
-	       (format pane "~S" (config-info-default-value v)))
-	     (slim:cell
-	       (format pane "~S" (config-info-valid-values-description v)))))
-	 db)
-	(slim:row (slim:col (slim:cell (format pane "~S" db)))))))
+	  (format pane "Value"))))
+    (cond ((not (hash-table-p db))
+	   (slim:row (slim:cell (format pane "Unknown Database Type ~A" db))))
+	  ((hash-table-empty-p db)
+	   (slim:row (slim:cell (format pane "No Entries"))))
+	  (t (maphash
+	      (lambda (k v)
+		(with-output-as-presentation
+		    (pane v 'inpect-config-info-presentation)
+		  (slim:row
+		    (slim:cell
+		      (format pane "~A" (package-name (symbol-package k))))
+		    (slim:cell
+		      (format-color pane *config-symbol-color* "~A"
+				    (symbol-name k)))
+		    (slim:cell
+		      (format pane "~S" (symbol-value k))))))
+	      db)))))
 
 (let (key dbname db)
   (defun display-database (frame pane)
@@ -126,11 +169,116 @@
     (bold (pane) (format-color pane *database-name-color* "~S~%~%" dbname))
     (format pane "~%Accessor Database~%")
     (with-end-of-line-action (pane :allow)
-      (write-database-to-stream pane (car db)))
-    (format pane "Variable Database~%")
+      (write-database-to-stream pane (car db) t))
+    (format pane "~%Variable Database~%")
     (with-end-of-line-action (pane :allow)
       (write-database-to-stream pane (cdr db))))
   (defun set-display-db (db-key db-name database)
     (setf key db-key
 	  dbname db-name
 	  db database)))
+
+(defun display-db-entry-values-column (pane config-info)
+  (slim:row 
+    (slim:cell
+      (italic (pane)
+	(format pane "Default Value:  ")))
+    (slim:cell
+      (monospaced (pane)
+	(format pane "~A" (config-info-default-value config-info))))
+    (slim:cell
+      (with-output-as-gadget (pane)
+	(make-pane 'push-button
+		   :label (format nil "RESET ~A" (config-info-place config-info))
+		   :activate-callback
+		   (lambda (gadget)
+		     (declare (ignore gadget))
+		     (defconfig:reset-computed-place
+		      (config-info-place config-info)
+		      :db (symbol-value
+			   (defconfig::config-info-db
+			    config-info)))
+		     (com-refresh-frame))))))
+  (slim:row
+    (slim:cell
+      (italic (pane)
+	(format pane "Previous Value:  ")))
+    (slim:cell
+      (monospaced (pane)
+	(format pane "~A" (config-info-prev-value config-info))))
+    (slim:cell
+      (with-output-as-gadget (pane)
+	(make-pane 'push-button
+		   :label (format nil "RESET ~A" (config-info-place config-info))
+		   :activate-callback
+		   (lambda (gadget)
+		     (declare (ignore gadget))
+		     (defconfig::reset-computed-place
+		      (config-info-place config-info)
+		      :db (symbol-value
+			   (defconfig::config-info-db
+			    config-info))
+		      :previous-value t)
+		     (com-refresh-frame)))))))
+
+(defun display-db-entry-fn (pane label name doc)
+  (slim:row
+    (slim:cell
+      (italic (pane)
+	(format pane "~A:  " label)))
+    (slim:cell
+      (monospaced (pane)
+	(format pane "~A" name))))
+  (slim:row
+    (slim:cell
+      (italic (pane)
+	(format pane "Documentation:  ")))
+    (slim:cell
+      (monospaced (pane)
+	(format pane "~A" doc)))))
+
+(defun display-db-entry-functions-column (pane config-info)
+  (flet ((get-fn-name (fnct)
+	   (multiple-value-bind (ig1 ig2 lamb)
+	       (function-lambda-expression fnct)
+	     (declare (ignore ig1 ig2))
+	     (if (listp lamb)
+		 (list (car lamb) (cadr lamb))
+		 lamb))))
+    (let* ((cfn (config-info-coercer config-info))
+	   (fn (config-info-predicate config-info))
+	   (doc (documentation fn 'function))
+	   (name (get-fn-name fn)))
+      (display-db-entry-fn pane "Predicate" name doc)
+      (when cfn
+	(let ((cdoc (documentation cfn 'function))
+	      (cname (get-fn-name cfn)))
+	  (display-db-entry-fn pane "Coercer" cname cdoc))))))
+
+(defun display-db-entry (pane config-info)
+  (slim:with-table (pane)
+    ;; one column for values, one for metadata, one for functions
+    (display-db-entry-values-column pane config-info)
+    ;; (display-db-entry-functions-column pane config-info)
+    ))
+
+(defun display-config-info (frame pane)
+  (let* ((info-obj (configurator-current-config-info frame)))
+    (format pane " ~%")
+    (slim:with-table (pane)
+      (slim:row
+	(slim:cell
+	  (format-color pane *config-symbol-color*
+			"~0,4T~A" (config-info-place info-obj))
+	  (format pane "  holds the value  ")
+	  (format-color pane *config-symbol-color*
+			"~A" (symbol-value (config-info-place info-obj))))))
+    ;; (with-output-as-gadget (pane)
+    ;;   (make-pane 'push-button-pane
+    ;; 		 :label "test button"
+    ;; 		 :activate-callback
+    ;; 		 (lambda (g)
+    ;; 		   (declare (ignore g))
+    ;; 		   (notify-user *application-frame* "Hi there!"))))
+    (format pane "~%~0,4T~A~%" (config-info-documentation info-obj))
+    (display-db-entry pane info-obj)))
