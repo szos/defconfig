8c3ebdf3c7f2c0751a19ce9b17bbc2e092964953
Nathan Shostek <szos@posteo.net>
2021-01-26 15:24:58 +0100

    Added database creation and management functions/macros
    specifically, we added a plist to store all defconfig databases.
    we added a condition to signal if a database already exists.
    we added the internal functions ADD-DB-TO-PLIST, DB-KEY-EXISTS-P, and
    GETDB.
    we added the external functions GET-DB, GET-DB-VAR, LIST-DBS, and
    DELETE-DB.
    we added the external macro DEFINE-DEFCONFIG-DB.

--- package.lisp
+++ package.lisp
@@ -4,12 +4,24 @@
   (:use #:cl)
   (:import-from #:trivial-cltl2 #:compiler-let)
   (:export #:defconfig
+	   
+	   ;; setters and access functions/macros
 	   #:setv
 	   #:setv-atomic
 	   #:with-atomic-setv
+	   #:reset-place
 	   #:config-info-search
+
+	   ;; errors
+	   #:config-error
 	   #:invalid-datum-error
 	   #:invalid-coerced-datum-error
 	   #:no-config-found-error
-	   #:make-config-database
-	   #:reset-place))
+	   #:database-already-exists-error
+
+	   ;; database creation and lookup
+	   #:define-defconfig-db
+	   #:get-db
+	   #:get-db-var
+	   #:list-dbs
+	   #:delete-db))
