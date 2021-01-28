708b461ccdbaea2e1713c9b41ea5d76c6d20e667
Nathan Shostek <szos@posteo.net>
2021-01-28 16:22:18 +0100

    added better tests for with-atomic-setv

--- tests/defconfig-tests.lisp
+++ tests/defconfig-tests.lisp
@@ -12,9 +12,10 @@
 (in-package :defconfig.test)
 
 (am:test clean
-  (am:is (and (boundp '*testing-db*)
-	      (get-db :testing)
-	      (delete-db :testing t))))
+  (am:is (or (and (boundp '*testing-db*)
+		  (get-db :testing)
+		  (delete-db :testing t))
+	     t)))
 
 (am:test makedb
   (define-defconfig-db *testing-db* :testing)
@@ -44,6 +45,33 @@
   (signals defconfig:no-config-found-error
     (setv *light-dark* 'dark)))
 
+(am:test fine-grained-w-a-s-signal-setv-wrapped-error
+  (defconfig *bounded-number* 0 :typespec '(integer 0 10)
+    :coercer (lambda (x) (if (stringp x) (parse-integer x) x))
+    :documentation "A number with the bounds 0 to 10 inclusive"
+    :tags '("bounded number" "integer") :db *testing-db*
+    :reinitialize t :regen-config t)
+  (is (= *bounded-number* 0))
+  (signals defconfig::setv-wrapped-error
+    (with-atomic-setv ()
+      (setv *bounded-number* 1
+  	    :db *testing-db*)
+      (error "testing-error"))))
+
+(am:test fine-grained-w-a-s-invalid-datum-error
+  (defconfig *bounded-number* 0 :typespec '(integer 0 10)
+    :coercer (lambda (x) (if (stringp x) (parse-integer x) x))
+    :documentation "A number with the bounds 0 to 10 inclusive"
+    :tags '("bounded number" "integer") :db *testing-db*
+    :reinitialize t :regen-config t)
+  (is (= *bounded-number* 0))
+  (signals defconfig:invalid-datum-error
+    (handler-case (with-atomic-setv ()
+		    (setv *bounded-number* 20
+			  :db *testing-db*))
+      (defconfig::setv-wrapped-error (c)
+	(error (defconfig::setv-wrapped-error-condition c))))))
+
 (am:test test-with-atomic-setv
   (defconfig *bounded-number* 0 :typespec '(integer 0 10)
     :coercer (lambda (x) (if (stringp x) (parse-integer x) x))
@@ -55,7 +83,7 @@
     (with-atomic-setv ()
       (setv *bounded-number* 1
   	    :db *testing-db*)
-       (error "testing-error")))
+      (error "testing-error")))
   (is (= *bounded-number* 0))
   (is (= 1
 	 (with-atomic-setv ()
@@ -67,6 +95,32 @@
 			  :db *testing-db*))
       (defconfig::setv-wrapped-error (c)
 	(error (defconfig::setv-wrapped-error-condition c))))))
+
+(am:test test-w-a-s-specific-errors
+  (defconfig *bounded-number* 0 :typespec '(integer 0 10)
+    :coercer (lambda (x) (if (stringp x) (parse-integer x) x))
+    :documentation "A number with the bounds 0 to 10 inclusive"
+    :tags '("bounded number" "integer") :db *testing-db*
+    :reinitialize t :regen-config t)
+  (defconfig *other-bounded-number* 0 :typespec '(integer 0 10)
+    :coercer (lambda (x) (if (stringp x) (parse-integer x) x))
+    :documentation "A number with the bounds 0 to 10 inclusive"
+    :tags '("bounded number" "integer") :db *testing-db*
+    :reinitialize t :regen-config t)
+  (with-atomic-setv (:handle-errors (defconfig::config-error))
+    (setv *other-bounded-number* 1
+	  *bounded-number* 1
+	  :db *testing-db*))
+  (is (and (equal *bounded-number* 1)
+	   (equal *other-bounded-number* 1)))
+  (signals simple-error
+    (with-atomic-setv (:handle-errors (defconfig::config-error))
+      (setv *other-bounded-number* 2
+	    *bounded-number* 2
+	    :db *testing-db*)
+      (error "ahhhh!!!")))
+  (is (and (equal *bounded-number* 2)
+	   (equal *other-bounded-number* 2))))
 
 (am:test test-setv-atomic
   (is (eql (setv *light-dark* 'dark
